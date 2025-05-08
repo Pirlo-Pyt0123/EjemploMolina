8933ee9f73a8ef0f78aef97b2bc830f4cfda54a5
Pirlo-Pyt0123 <148081433+Pirlo-Pyt0123@users.noreply.github.com>
2025-05-08 18:05:48 -0400

    Bloques Listos

---
+++ Source/EjemploMolina/BloqueB.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bloque.h"
+#include "BloqueB.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EJEMPLOMOLINA_API ABloqueB : public ABloque
+{
+	GENERATED_BODY()
+	
+};
